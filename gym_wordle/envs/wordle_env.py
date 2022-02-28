755dbf45354e54d7774399bb56d5426c6afb76fa
Afzal Pawaskar <2928898+afzalmushtaque@users.noreply.github.com>
2022-02-28 15:09:16 +0530

    fixed bugs

--- gym_wordle/envs/wordle_env.py
+++ gym_wordle/envs/wordle_env.py
@@ -1,3 +1,4 @@
+from math import dist
 import gym
 from gym import spaces
 import numpy as np
@@ -17,33 +18,11 @@ colorama.init(autoreset=True)
 GAME_LENGTH = 6
 WORD_LENGTH = 5
 
-# ALPHABET_STATUS_TO_INT_LOOKUP = {}
-# INT_TO_ALPHABET_STATUS_LOOKUP = {}
-
-# for i in range(4):
-#     for j in range(27):
-#         ALPHABET_STATUS_TO_INT_LOOKUP[(i, j)] = 27 * i + j
-#         INT_TO_ALPHABET_STATUS_LOOKUP[27 * i + j] = {'status': i, 'alphabet': j}
-
-
 # load words and then encode
 filename = pkg_resources.resource_filename(
     'gym_wordle',
     'data/5_words.txt'
 )
-
-def custom_distance(source, target):
-    distance = 0
-    for index, char in enumerate(source):
-        if char in target:
-            # alphabet present in hidden word
-            if char!=target[index]:
-                # but at the wrong location
-                distance += 1
-        else:
-            # alphabet not present in hidden word
-            distance += 2
-    return distance
 
 def encodeToStr(encoding):
     string = ""
@@ -60,22 +39,8 @@ def strToEncode(lines):
 
 with open(filename, "r") as f:
     WORDS = strToEncode(f.readlines())
-    WORDS = [WORDS[x] for x in [ 5934,  1408,  5487,  2871, 12660, 12612,  4394,  2145,  3507,
-        6281,  2322, 13812,  2695, 10663,  9023,  6413, 14815,  9200,
-         346, 12534, 12937,  3055,  8307,  9481, 12548, 12499, 11114,
-        9218, 10979, 13564,  5067,  3354,  6375,  4079,  8779,  3440,
-       11819,  5872,  9188,  9712,  7459,  3329,  9195, 14081, 14330,
-       13477,   771, 10790,  4887,  3538,  4578,  2594,  1111,  2142,
-         516,  7287, 10601,  9404, 14485, 13777, 10377, 12240,  2687,
-        9043,   418,  4581,   932,  1495,  2859,   148,  8626,  6288,
-        4483,  6015,   468,  7038,  9845,   562, 14139,  5879,   394,
-       11004,  4948,  8004,  4005,  6121, 14364, 10350, 14237,  5168,
-       13685, 12396,  4451,  9456,  9503, 14883,  9217,  6768,  4920,
-        5791]
-    ]
-    # random.shuffle(WORDS)
-    # WORDS = WORDS[:1]
 
+VOCAB_SIZE = len(WORDS)
 
 class WordleEnv(gym.Env):
     """
@@ -120,32 +85,38 @@ class WordleEnv(gym.Env):
         super(WordleEnv, self).__init__()
         self.action_space = spaces.MultiDiscrete([26] * WORD_LENGTH)
         self.observation_space = spaces.Box(low=-1, high=2, shape=(WORD_LENGTH, 26,))
+        self.syllabus = list(np.arange(100))
+        
 
+    def expand_syllabus(self):
+        if len(self.syllabus) < VOCAB_SIZE:
+            logging.info('Expanding syllabus to ' + str(self.syllabus[-1] + 1) + ' words...')
+            self.syllabus.append(self.syllabus[-1] + 1)
+        else:
+            logging.debug('Skipping syllabus expansion as vocab limit ' + str(self.syllabus[-1] + 1) + ' reached...')
+    
+   
     def reset(self, seed: Optional[int] = None):
         # super().reset(seed=seed)
-        if seed:
+        if seed is not None:
             self.hidden_word = random.choice([WORDS[x] for x in seed])
         else:
-            self.hidden_word = random.choice(WORDS)
+            self.hidden_word = random.choice([WORDS[x] for x in self.syllabus])
         # self.guesses_left = GAME_LENGTH
         self.board_row_idx = 0
         self.state = np.negative(np.ones(shape=(WORD_LENGTH, 26), dtype=int))
+        self.prev_state = np.negative(np.ones(shape=(WORD_LENGTH, 26), dtype=int))
         self.board = np.negative(np.ones(shape=(GAME_LENGTH, WORD_LENGTH,), dtype=int))
         self.guesses = []
         # self.scores = [self.state.sum()]
         return self._get_obs()
 
     def step(self, action):
-        # action = WORDS[action]
-        # assert self.action_space.contains(action)
-
-        # Action must be a valid word
-        # if not tuple(action) in WORDS:
-        #     return self._get_obs(), -11, True, {}
-        # update game board
         hw = list(self.hidden_word)
         solved_indexes = []
+        distance = 0
         for idx, char in enumerate(action):
+            self.prev_state = self.state.copy()
             if char == self.hidden_word[idx]:
                 # in the correct location
                 self.board[self.board_row_idx, idx] = 2
@@ -154,17 +125,22 @@ class WordleEnv(gym.Env):
                 self.state[idx, char] = 2 
                 hw.remove(char)
                 solved_indexes.append(idx)
-
+                if self.state[idx, char]!=self.prev_state[idx, char]:
+                    distance -= 10
         for idx, char in enumerate(action):
             if idx in solved_indexes:
                 continue
+            self.prev_state = self.state.copy()
             if char not in hw:
-                logging.debug('Alphabet "' + chr(97 + char) + '" does not exist in the remaning word at any non solved index. Rejecting it across the word.')
+                logging.debug('Alphabet "' + chr(97 + char) + '" does not exist in the remaining word at any non solved index. Rejecting it across the word.')
                 for i in range(WORD_LENGTH):
-                    if i not in solved_indexes:
-                        self.state[i, char] = 0
+                    self.state[:, char] = np.where(self.state[:, char]==-1, 0, self.state[:, char])
+                    # if i not in solved_indexes:
+                    #     self.state[i, char] = 0
+                distance += 0.2
                 if not idx in solved_indexes:
                     self.board[self.board_row_idx, idx] = 0
+                
             else:
                 # alphabet is present in the word
                 if char != self.hidden_word[idx]:
@@ -173,7 +149,12 @@ class WordleEnv(gym.Env):
                     hw.remove(char)
                     self.state[idx, char] = 0
                     self.state[:, char] = np.where(self.state[:, char]==-1, 1, self.state[:, char])
-                    
+                    distance += 0.1
+            # check for changes
+            if self.state[idx, char]==self.prev_state[idx, char]:
+                # did not gain new information    
+                distance += 0.1
+
         deductions_pending = True
         while(deductions_pending):
             deductions_pending = False
@@ -187,29 +168,31 @@ class WordleEnv(gym.Env):
                             deductions_pending = True
                             break # can only happen once with an alphabet so no need to continue the for loop
         
-        possible_alphabets = []
-        known_alphabets = []
-        for i in range(26):
-            if (self.state[:, i]==1).any():            
-                possible_alphabets.append(i)
-            for j in range(5):
-                if self.state[j, i]==2:            
-                    known_alphabets.append(i)
-        if len(possible_alphabets) == 5 - len(known_alphabets):
-            for i in range(26):
-                if i not in possible_alphabets and i not in known_alphabets:
-                    logging.debug('Eliminating alphabet "' + chr(97 + i) + '" from consideration as all correct alphabets are either known or possible.')
-                    self.state[:, i] = 0
-        # reward = self.score - self.previous_score
-        # reward -= 1/6
+        # possible_alphabets = []
+        # known_alphabets = []
+        # for i in range(26):
+        #     if (self.state[:, i]==1).any():            
+        #         possible_alphabets.append(i)
+        #     for j in range(5):
+        #         if self.state[j, i]==2:            
+        #             known_alphabets.append(i)
+        # if len(possible_alphabets) == 5 - len(known_alphabets):
+        #     for i in range(26):
+        #         if i not in possible_alphabets and i not in known_alphabets:
+        #             logging.debug('Eliminating alphabet "' + chr(97 + i) + '" from consideration as all correct alphabets are either known or possible.')
+        #             self.state[:, i] = 0
         
         # update guesses remaining tracker
         self.board_row_idx += 1
 
         # update previous guesses made
         self.guesses.append(action)
-
+        
+        assert -50 <= np.round(distance, 6) <= 1.5
+        # scaled_distance = (distance + 50) / (1.5 + 50)
+        # reward = -((scaled_distance * 2) - 1)
         # check to see if game is over
+        reward = -distance / 50
         if all(self.board[self.board_row_idx - 1, :] == 2):
             done = True
         else:
@@ -217,19 +200,13 @@ class WordleEnv(gym.Env):
                 done = True
             else:
                 done = False
-        # if done:
-            # reward += self.board[self.board_row_idx, :].sum()
-        # self.scores.append(self.state.sum())
-        # reward = -custom_distance(encodeToStr(action), encodeToStr(self.hidden_word))
-        reward = -custom_distance(action, self.hidden_word)
-        # reward = self.scores[-1] - self.scores[-2]
-        # reward -= 40/6
+        
+        self.prev_state = self.state.copy()
         return self._get_obs(), reward, done, {}
 
     def _get_obs(self):
         return self.state
     
-
     def render(self, mode="human"):
         assert mode in ["human"], "Invalid mode, must be \"human\""
         print('###################################################')
@@ -248,8 +225,10 @@ class WordleEnv(gym.Env):
         print()
 
 if __name__ == "__main__":
+    import sys
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     env = WordleEnv()
-    obs = env.reset()
+    obs = env.reset(seed=[10])
     step = 0
     print('Hidden word:')
     print(encodeToStr(env.hidden_word))
